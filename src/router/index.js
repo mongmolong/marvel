6c706afe3197a1e1a0712aed3dda1c1b881b067c
mongmolong <143096636+mongmolong@users.noreply.github.com>
2024-11-06 15:54:43 +0900

    retry

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/home.vue'; // 각 페이지 컴포넌트 경로에 맞게 수정
+import Home from '../views/Home.vue'; // 각 페이지 컴포넌트 경로에 맞게 수정
 import Marvel from '../views/Random.vue';
 import Movie from '../views/Search.vue';
 
